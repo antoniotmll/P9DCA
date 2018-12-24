6e20b84934168fb3c00432f25a541759f46d3295
Antonio Tomas <antoniotmll@gmail.com>
2018-12-24 16:20:50 +0100

    Cambio 1

--- sondasrmi/Registro.java
+++ sondasrmi/Registro.java
@@ -12,7 +12,7 @@ public class Registro {
 	//@SuppressWarnings("deprecation")
 	public static void main (String[] args) {
 
-		String URLRegistro = "";
+		String URLRegistro = "dato";
 
         try {
 
